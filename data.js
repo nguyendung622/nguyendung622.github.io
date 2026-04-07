4e2f1e3dadbb632895027cdb1f35af8ec12b175d
nguyendung622@gmail.com <AD113688>
2026-04-07 11:57:21 +0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,70 @@
+﻿// ============================================================
+// FILE DU LIEU CA NHAN - CAP NHAT THONG TIN O DAY
+// ============================================================
+
+const profileData = {
+  // --- THONG TIN CA NHAN ---
+  name: "Nguyễn Dũng",
+  nameEn: "Nguyen Dung",
+  title: "Giảng viên",
+  department: "Khoa Công nghệ Thông tin",
+  university: "Trường Đại học Khoa học, Đại học Huế",
+  universityEn: "University of Sciences, Hue University",
+  address: "77 Nguyễn Huệ, TP. Huế, Việt Nam",
+
+  // --- LIEN HE ---
+  emails: [
+    "ndung@husc.edu.vn",
+    "nguyendung@hueuni.edu.vn"
+  ],
+  orcid: "0009-0000-4510-7504",
+  orcidUrl: "https://orcid.org/0009-0000-4510-7504",
+
+  // --- GIOI THIEU ---
+  bio: "Tôi là giảng viên tại Khoa Công nghệ Thông tin, Trường Đại học Khoa học, Đại học Huế. Nghiên cứu của tôi tập trung vào các lĩnh vực trí tuệ nhân tạo, học máy và các ứng dụng thực tiễn trong xử lý dữ liệu.",
+
+  // --- HUONG NGHIEN CUU ---
+  researchInterests: [
+    "Phát hiện đối tượng",
+    "Học sâu",
+    "Thị giác máy tính",
+    "Transformer",
+    "Xử lý ảnh",
+    "Học máy",
+    "IoT"
+  ],
+
+  // --- GIANG DAY ---
+  teaching: [
+    {
+      name: "Thiết kế và Quản trị Website Báo chí",
+      url: "http://tkweb.d-blog.xyz",
+      description: "Xây dựng, thiết kế và quản trị website báo chí trực tuyến"
+    },
+    {
+      name: "XML và Ứng dụng",
+      url: "http://xml.d-blog.xyz",
+      description: "Ngôn ngữ XML và các ứng dụng trong lưu trữ, trao đổi dữ liệu"
+    },
+    {
+      name: "Lập trình ứng dụng cho thiết bị di động",
+      url: null,
+      description: "Phát triển ứng dụng trên nền tảng Android và iOS"
+    }
+  ],
+
+  // --- CONG TRINH NGHIEN CUU ---
+  // Du lieu bai bao se duoc nap tu Google Sheet trong index.html
+  publications: [],
+
+  // --- HOC VAN ---
+  education: [],
+
+  // --- LIEN KET NGOAI ---
+  links: {
+    googleScholar: "https://scholar.google.com/citations?user=bWXae6IAAAAJ&hl=vi",
+    researchgate: "https://www.researchgate.net/profile/Dung-Nguyen-401",
+    github: "",
+    linkedin: ""
+  }
+};
